b5393a8c6ff695834895ab32249d2e8f5696d489
sivakavin-c <kavinsindhu3@gmail.com>
2022-08-26 13:35:25 +0530

    Handling User Authentication

---
+++ HandlingUserAuthentication/src/main/java/com/simplilearn/demo/UserAuthentication.java
@@ -0,0 +1,16 @@
+package com.simplilearn.demo;
+
+public class UserAuthentication {
+	public String getUserName() {
+		return "abcd";
+	}
+	public String getEmail() {
+		return "abcd@gmail.com";
+	}
+	public int getPhnNum() {
+		return 987654321;
+	}
+	public String getPassword() {
+		return "abcd@123";
+	}
+}
