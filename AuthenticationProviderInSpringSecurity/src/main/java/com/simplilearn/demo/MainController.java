13a2ca129d2af15f85e75e0349803c4853812223
sivakavin-c <kavinsindhu3@gmail.com>
2022-08-29 17:50:31 +0530

    Implementing spring security with authentication

---
+++ AuthenticationProviderInSpringSecurity/src/main/java/com/simplilearn/demo/MainController.java
@@ -0,0 +1,26 @@
+package com.simplilearn.demo;
+
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @RequestMapping("/")
+    public String hello(){
+        return "Hello World";
+    }
+
+    @RequestMapping("/protected")
+    public String protectedHello(){
+        return "Hello World, from User";
+    }
+
+    @RequestMapping("/admin")
+    public String admin(){
+        return "Hello World from admin";
+    }
+
+}
